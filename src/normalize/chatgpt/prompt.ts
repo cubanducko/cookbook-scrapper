35380ffbd2ca99e30e5562013a8d92cb46087fbe
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2024-01-07 20:07:09 +0100

    feat: prompt adjustments

--- src/normalize/chatgpt/prompt.ts
+++ src/normalize/chatgpt/prompt.ts
@@ -20,6 +20,7 @@ export const normalizerPrompt = `
       cuisine: CuisineInfluence[];
       time: PreparationTime;
       difficulty: DifficultyLevel;
+      moment: Position;
       others: (NutrientContent | HealthRelated)[];
     };
     keywords: string[]
@@ -39,17 +40,21 @@ export const normalizerPrompt = `
   
   type DifficultyLevel = "Beginner" | "Intermediate" | "Advanced" | "Expert";
 
+  type Position = "Starter" | "Main" | "Dessert" | "Drink" | "Snack" | "Breakfast" | "Brunch" | "Lunch" | "Dinner" | "Appetizer" | "Side" | "Sauce" | "Other";
+
   You must maintain the original HTML language.
   Some HTML contain multiple Recipe elements.
   Title should be generated per Recipe. The title should convey the recipe. Be creative and witty, without overdoing it.
-  You should identify the following information: language, step-by-step instructions, list of ingredients, total preparation time, and difficulty level.
+  You should identify the following information: language, step-by-step instructions, list of ingredients with their quantities, total preparation time, and difficulty level.
+  Always include the quantities of the ingredients.
   If one of the extracted steps contains multiple steps, you should split it into multiple steps.
   Additionally, it should classify the recipe based on various tags.
   Difficulty should be a number between 1 and 10.
   10 is something that takes days to prepare and requires a lot of skill.
   1 is something quick that requires little ingredientes and little to no prep work.
   All tags categories, except other, must be filled with something.
-  Keywords is an array that contains the different ingredients (without their quantity) and simplified to a word at most. Use the most relevant ones. In their original language.
+  Keywords is an array that contains the different ingredients (without their quantity) and simplified to the most relevant word. For some foods, for example cheeses, the most relevant work is the type.
+  For example, for feta cheese the most relevant one is feat, not cheese. Use the most relevant ones always. In their original language.
   Use only the HTML provided in the input, do not include additional information.
   Consider variations in HTML structures and ensure your can handle different page layouts.
   Only reply with the JSON, do not include any other information.
