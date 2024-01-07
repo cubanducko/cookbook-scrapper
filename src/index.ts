be2daaa1aefbfd2de0f51531c2f17cf1ad5958b4
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2024-01-07 12:15:08 +0100

    feat: add basic scrapper example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import { PlaywrightCrawler } from "crawlee";
+import { chromium } from "playwright-extra";
+import stealthPlugin from "puppeteer-extra-plugin-stealth";
+
+chromium.use(stealthPlugin());
+
+async function main() {
+  const crawler = new PlaywrightCrawler({
+    launchContext: {
+      launcher: chromium,
+      launchOptions: {
+        headless: false,
+      },
+    },
+
+    // Stop crawling after several pages
+    maxRequestsPerCrawl: 50,
+
+    async requestHandler({ request, enqueueLinks, log }) {
+      log.info(request.url);
+      // Add all links from page to RequestQueue
+      await enqueueLinks();
+    },
+  });
+
+  // Run the crawler with initial request
+  await crawler.run(["https://crawlee.dev"]);
+}
+
+main();
